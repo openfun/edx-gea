d4ad5748c65bdd6a6f58e714529c874cea5e9ec9
Julien Maupetit <julien@maupetit.net>
2018-03-21 16:13:22 +0100

    feat(package) add setup script and configuration
    Nota bene:
    
    edx-gea is supposed to work integrated in an Open edX instance where
    xblock-utils is already installed. xblock-utils is required by edx-gea
    but cannot be defined as a base requirement as it is not published in
    PyPI. To avoid breaking our packaging CI, we define xblock-utils as an
    extra requirement labeled "edx".

--- setup.py
+++ setup.py
@@ -1,38 +1,5 @@
-"""Setup for gea XBlock."""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
 
-import os
-from setuptools import setup, find_packages
-
-def package_data(pkg, roots):
-    """Generic function to find package_data.
-    All of the files under each of the `roots` will be declared as package
-    data for package `pkg`.
-
-    """
-    data = []
-    for root in roots:
-        for dirname, _, files in os.walk(os.path.join(pkg, root)):
-            for fname in files:
-                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
-    return {pkg: data}
-
-setup(
-    name='edx-gea',
-    version='0.1',
-    description='An XBlock to grade external activities',
-    author="FUN",
-    url="https://github.com/openfun/edx-gea",
-    zip_safe=False,
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'XBlock',
-        'xblock-utils',
-    ],
-    entry_points={
-        'xblock.v1': [
-            'edx_gea = edx_gea:GradeExternalActivityXBlock',
-        ]
-    },
-    package_data=package_data("edx_gea", ["static", "public", ]),
-)
+setup()
